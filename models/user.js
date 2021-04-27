d0c273c407608a28374ccfd555ea9904bb631a2e
Hyeonju Lee <winnerhj1109@naver.com>
2021-04-27 23:11:31 +0900

    #10 모델 생성

---
+++ models/user.js
@@ -0,0 +1,59 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      idx: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING(128),
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING(20),
+        unique: true,
+        validate: {
+          notNull: true,
+        },
+      },
+      salt: {
+        allowNull: false,
+        type: DataTypes.STRING(200),
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING(200),
+        validate: {
+          notNull: true,
+        },
+      },
+    },
+    {
+      paranoid: true,
+      timestamps: true,
+    },
+  );
+
+  User.associate = (models) => {
+    /** 1 : N   User : Journey */
+    User.hasMany(models.Journey, {
+      foreignKey: { name: 'userIdx', allowNull: false },
+      onDelete: 'cascade',
+    });
+    /** 1 : N   User : Todo */
+    User.hasMany(models.ToDo, {
+      foreignKey: { name: 'userIdx', allowNull: false },
+      onDelete: 'cascade',
+    });
+  };
+
+  return User;
+};
