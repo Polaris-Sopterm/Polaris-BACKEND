8a0f3b0119135c112830b852a2a19efd8696bb4b
uju_sy <lsyeon1997@ajou.ac.kr>
2021-05-03 23:35:09 +0900

    #리뷰 반영
    - cloudbuild.yaml에 db migration 추가

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = import('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'user',
@@ -19,20 +21,42 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         allowNull: false,
         type: DataTypes.STRING(20),
-        unique: true,
         validate: {
           notNull: true,
         },
       },
-      salt: {
-        allowNull: false,
-        type: DataTypes.STRING(200),
-      },
       password: {
         allowNull: false,
         type: DataTypes.STRING(200),
+        set(value) {
+          const salt = bcrypt.genSaltSync();
+          this.setDataValue('password', bcrypt.hashSync(value, salt));
+        },
         validate: {
           notNull: true,
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE(3),
+        validate: {
+          isDate: true,
+          notNull: true,
+        },
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE(3),
+        validate: {
+          isDate: true,
+          notNull: true,
+        },
+      },
+      deletedAt: {
+        allowNull: true,
+        type: DataTypes.DATE(3),
+        validate: {
+          isDate: true,
         },
       },
     },
@@ -42,11 +66,25 @@ module.exports = (sequelize, DataTypes) => {
     },
   );
 
+  User.prototype.validatePasswordHash = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
   User.associate = (models) => {
     /** 1 : N   User : Journey */
-    User.hasMany(models.Journey);
+    User.hasMany(models.Journey, {
+      foreignKey: 'userIdx',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      sourceKey: 'idx',
+    });
     /** 1 : N   User : Todo */
-    User.hasMany(models.ToDo);
+    User.hasMany(models.ToDo, {
+      foreignKey: 'userIdx',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+      sourceKey: 'idx',
+    });
   };
 
   return User;
