5810d63eebfe776d633ec165d26d32eea39a3845
ujusy <lsyeon1997@ajou.ac.kr>
2022-02-09 22:38:51 +0900

    #176 마지막 주차 정보 api 5년 데이터 가져오도록 로직 변경

--- routes/weekNo/v0.js
+++ routes/weekNo/v0.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-restricted-syntax, no-await-in-loop */
+
 const express = require('express');
 
 const asyncRoute = require('../../utils/asyncRoute');
 const { getWeekOfMonth } = require('../../utils/weekCalculation');
-const { HttpBadRequest, Errors, HttpInternalServerError } = require('../../middlewares/error');
+const { Errors, HttpInternalServerError } = require('../../middlewares/error');
 
 /**
  * @param {Request} req
@@ -27,49 +29,70 @@ const getWeekNo = async (req, res) => {
  * @returns {Promise<*>}
  */
 const lastMonthWeekNo = async (req, res) => {
-  const { year, month } = req.body;
-
-  if (!year) throw new HttpBadRequest(Errors.WEEK_NO.YEAR_MISSING);
-  if (!month) throw new HttpBadRequest(Errors.WEEK_NO.MONTH_MISSING);
-
   let weekNo;
   let lastDate;
   let lastDay;
   let date;
+
+  const resBody = {
+    data: [],
+  };
+
   try {
-    lastDate = new Date(year, month, 0);
-    lastDay = lastDate.getDate();
-    date = `${year}-${month}-${lastDay}`;
+    const today = new Date();
+    const todayYear = today.getFullYear();
 
-    weekNo = await getWeekOfMonth(new Date(date));
+    const fiveYears = [todayYear - 2, todayYear - 1, todayYear, todayYear + 1, todayYear + 2];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    if (weekNo.weekNo === 1) {
-      while (weekNo.weekNo === 1) {
-        lastDay -= 1;
+    let index;
+    let tmpMonth;
+    for (const year of fiveYears) {
+      index = fiveYears.indexOf(year);
+
+      if (JSON.stringify(resBody.data[index]) !== '[]') {
+        resBody.data.push({ year });
+      }
+
+      for (const month of months) {
+        tmpMonth = months.indexOf(month) + 1;
+
+        if (resBody.data[index][month] !== '{}') {
+          resBody.data[index][month] = {};
+        }
+
+        lastDate = new Date(year, tmpMonth, 0);
+        lastDay = lastDate.getDate();
+
         date = `${year}-${month}-${lastDay}`;
 
-        // eslint-disable-next-line no-await-in-loop
         weekNo = await getWeekOfMonth(new Date(date));
 
-        if (weekNo.weekNo !== 1) break;
+        if (weekNo.weekNo === 1) {
+          while (weekNo.weekNo === 1) {
+            lastDay -= 1;
+            date = `${year}-${tmpMonth}-${lastDay}`;
+
+            weekNo = await getWeekOfMonth(new Date(date));
+
+            if (weekNo.weekNo !== 1) break;
+          }
+        }
+        resBody.data[index][month] = { weekNo: weekNo.weekNo };
       }
     }
   } catch (e) {
     throw new HttpInternalServerError(Errors.SERVER.UNEXPECTED_ERROR, e);
   }
 
-  const resBody = {
-    weekNo: weekNo.weekNo,
-  };
-
   return res.status(200).json(resBody);
 };
 
 const router = express.Router();
 
-router.get('/:date', asyncRoute(getWeekNo));
+router.get('/lastWeekOfMonth', asyncRoute(lastMonthWeekNo));
 
-router.post('/lastWeekOfMonth', asyncRoute(lastMonthWeekNo));
+router.get('/:date', asyncRoute(getWeekNo));
 
 module.exports = {
   router, getWeekNo, lastMonthWeekNo,
